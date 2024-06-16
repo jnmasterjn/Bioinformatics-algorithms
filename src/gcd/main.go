bbab68393ed1361c9027dab08f6555c2dc93ab3e
Janice <janice0975751227@gmail.com>
2024-06-17 01:32:40 +0800

    start gcd

---
+++ src/gcd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+// TrivialGCD(a,b)
+//     d ← 1
+//     m ← Min2(a,b)
+//     for every integer p between 1 and m
+//         if p is a divisor of both a and b
+//             d ← p
+//     return d
+
+func main() {
+	fmt.Println("hi")
+}
+
+func Min2(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func TrivialGCD(a, b int) int {
+	d := 1
+	m := Min2(a, b)
+
+}
