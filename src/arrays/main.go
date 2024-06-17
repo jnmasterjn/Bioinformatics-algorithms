246a58aaedcf2a4bf23c0cda0af5624817434593
Janice <janice0975751227@gmail.com>
2024-06-18 00:41:55 +0800

    start arrays

---
+++ src/arrays/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("arrays")
+
+	var list [6]int
+
+	list[0] = 8 //first index set to 8
+	fmt.Println(list)
+	fmt.Println(len(list))
+}
+
+func FactorialArray(n int) []int {
+	fmt.Print("working on")
+}
