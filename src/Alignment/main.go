35f9f8ad766002d58aa84a81e87a25192e255136
Janice <janice0975751227@gmail.com>
2024-06-28 12:24:05 -0400

    adjust position of files

---
+++ src/Alignment/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Sequence alignment!")
+}
+
+func Hemoglobin() {
+	ZebraFish := ReadFASTAFile("Data/Hemoglobin/Danrio_rerio_hemoglobin.fasta")
+
+	Cow := ReadFASTAFile("Data/Hemoglobin/Bos_taurus_hemoglobin.fasta")
+
+	Gorilla := ReadFASTAFile("Data/Hemoglobin/Gorilla_gorilla_hemoglobin.fasta")
+
+	Human := ReadFASTAFile("Data/Hemoglobin/Homo_sapiens_hemoglobin.fasta")
+
+	match := 1.0
+	mismatch := 1.0
+	gap := 3.0
+}
