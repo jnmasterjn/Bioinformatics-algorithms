6526ed3577c59b15e1e9dac63f5506cf4b8c6b74
Janice <janice0975751227@gmail.com>
2024-06-28 11:52:05 -0400

    global alignment

--- Alignment/globalAlignment.go
+++ Alignment/globalAlignment.go
@@ -1,12 +1,91 @@
 package main
 
-import "fmt"
-
 type Alignment [2]string
 
-//GlobalAlignment takes two strings, along with match, mismatch, and gap scores.
-//It returns a maximum score global alignment of the strings corresponding to these penalties.
+// GlobalAlignment takes two strings, along with match, mismatch, and gap scores.
+// It returns a maximum score global alignment of the strings corresponding to these penalties.
 func GlobalAlignment(str1, str2 string, match, mismatch, gap float64) Alignment {
 	var a Alignment
+
+	//The final strings
+	ms1 := ""
+	ms2 := ""
+
+	//Make the score table
+	lcM := GlobalScoreTable(str1, str2, match, mismatch, gap)
+
+	//row and column
+	r := len(lcM) - 1
+	c := len(lcM[0]) - 1
+
+	//we want to end the loop when we reach the matrix[0][0] (well)
+	//starts from the end (sink)
+	for r > 0 && c > 0 {
+		num := lcM[r][c]
+
+		//This part needs to be modified
+		// if going diagonal increases the score, matched
+		if lcM[r-1][c-1]+match == num && str1[r-1] == str2[c-1] {
+			//Add the letters
+			ms1 = string(str1[r-1]) + ms1
+			ms2 = string(str2[c-1]) + ms2
+			r--
+			c--
+			//add letter to both ms1 and ms2 if match
+		} else if lcM[r-1][c]-gap == num {
+			ms1 = string(str1[r-1]) + ms1
+			ms2 = "-" + ms2
+			r--
+			//add dash to ms1, add letter to ms2
+		} else if lcM[r][c-1]-gap == num {
+			ms2 = string(str2[c-1]) + ms2
+			ms1 = "-" + ms1
+			c--
+		} else { //mismatch
+			//add the different letters to corresponding message
+			ms1 = string(str1[r-1]) + ms1
+			ms2 = string(str2[c-1]) + ms2
+			r--
+			c--
+
+		}
+
+	}
+
+	//Compensating for the dashes at the beginning 😭 😭😭😭😭😭
+	for r > 0 {
+		ms1 = "-" + ms1
+		ms2 = string(str2[r]) + ms2
+		r--
+	}
+	for c > 0 {
+		ms2 = "-" + ms2
+		ms1 = string(str1[c]) + ms1
+		c--
+	}
+
+	//Setting the alignment variable
+	a[0] = ms1
+	a[1] = ms2
+
 	return a
+}
+
+func Max(x ...float64) float64 {
+	var max float64
+	for i, val := range x {
+		if i == 0 || val > max {
+			max = val
+		}
+	}
+	return max
+}
+
+func Matrix(len1, len2 int) [][]float64 {
+	arr := make([][]float64, len1)
+
+	for i := 0; i < len1; i++ {
+		arr[i] = make([]float64, len2)
+	}
+	return arr
 }
--- Alignment/globalAlignmentScore.go
+++ Alignment/globalAlignmentScore.go
@@ -1,7 +1,59 @@
 package main
 
-//GlobalScoreTable takes two strings and alignment penalties. It returns a 2-D array
-//holding dynamic programming scores for global alignment with these penalties.
+// GlobalScoreTable takes two strings and alignment penalties. It returns a 2-D array
+// holding dynamic programming scores for global alignment with these penalties.
 func GlobalScoreTable(str1, str2 string, match, mismatch, gap float64) [][]float64 {
-	return [][]float64{}
+
+	fill := Matrix(len(str1)+1, len(str2)+1)
+
+	for row := 1; row < len(str1)+1; row++ {
+		fill[row][0] = float64(row) * gap * (-1)
+	}
+
+	for col := 1; col < len(str2)+1; col++ {
+		fill[0][col] = float64(col) * gap * (-1)
+	}
+
+	//Calculating the values inside the matrixs
+	for row := 1; row <= len(str1); row++ {
+		for col := 1; col <= len(str2); col++ {
+
+			//Calculate values for the three directions
+			up := fill[row-1][col] - gap
+			left := fill[row][col-1] - gap
+			diag := fill[row-1][col-1]
+
+			//Two cases: match or mismatch
+			if str1[row-1] == str2[col-1] { //match
+				diag += match
+			} else {
+				diag -= mismatch
+			}
+
+			//Maximum score of three directions
+			max3 := Max(diag, left, up)
+			fill[row][col] = max3
+		}
+	}
+
+	return fill
+}
+
+func Matrix(len1, len2 int) [][]float64 {
+	arr := make([][]float64, len1)
+
+	for i := 0; i < len1; i++ {
+		arr[i] = make([]float64, len2)
+	}
+	return arr
+}
+
+func Max(x ...float64) float64 {
+	var max float64
+	for i, val := range x {
+		if i == 0 || val > max {
+			max = val
+		}
+	}
+	return max
 }
