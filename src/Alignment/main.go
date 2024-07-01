dd4c8bfc222924dac4ff288c2ff3ff239951e795
Janice <janice0975751227@gmail.com>
2024-07-01 11:27:59 -0400

    add on main.go

--- src/Alignment/main.go
+++ src/Alignment/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // func main() {
 // 	fmt.Println("Sequence alignment!")
 // }
@@ -16,4 +18,20 @@ func Hemoglobin() {
 	match := 1.0
 	mismatch := 1.0
 	gap := 3.0
+}
+
+func SARSAlignment() {
+
+	//read in 2 files
+	fmt.Println("Reading in genome files.")
+	sars1 := ReadFASTAFile("Data/Coronaviruses/SARA-CoV_genome.fasta")
+	sars2 := ReadFASTAFile("Data/Coronaviruses/SARA-CoV-2_genome.fasta")
+
+	fmt.Println("Files read. Now, aligning genomes")
+
+	GlobalAlignment(sars1, sars2, 1, 1, 2)
+
+	WriteAlignmentToFASTA(sarsAlignment, "Output/sarasGenoneAlignment.txt")
+
+	fmt.Println("Alignment written to files")
 }
