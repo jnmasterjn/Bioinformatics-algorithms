8f10d0d182c18908eec899cf614c56b72ff1fdaf
Janice <janice0975751227@gmail.com>
2024-07-02 12:30:54 -0400

    edit dis

--- src/Alignment/editDistance.go
+++ src/Alignment/editDistance.go
@@ -1,8 +1,54 @@
 package main
 
-//EditDistance takes two strings as input. It returns the Levenshtein distance
-//between the two strings; that is, the minimum number of substitutions, insertions, and deletions
-//needed to transform one string into the other.
+// EditDistance takes two strings as input. It returns the Levenshtein distance
+// between the two strings; that is, the minimum number of substitutions, insertions, and deletions
+// needed to transform one string into the other.
 func EditDistance(str1, str2 string) int {
-	return 0
+	dist := Matrix(len(str1)+1, len(str2)+1)
+
+	for row := 1; row < len(str1)+1; row++ {
+		dist[row][0] = row
+	}
+
+	for col := 1; col < len(str2)+1; col++ {
+		dist[0][col] = col
+	}
+
+	for row := 1; row <= len(str1); row++ {
+		for col := 1; col <= len(str2); col++ {
+
+			//If the string's letters are the same, then the diagonal value will not increase
+			if str1[row-1] == str2[col-1] {
+				dist[row][col] = dist[row-1][col-1]
+			} else {
+				//🌪️🌪️🌪️🌪️🌪️🌪️🌪️🌪️🌪️🌪️🌪️🌪️🌪️🌪️🌪️
+				//Otherwise, find the minimum of the diagonal, left index, and upwards index
+				dist[row][col] = min(dist[row-1][col], dist[row][col-1], dist[row-1][col-1]) + 1
+			}
+		}
+	}
+
+	return dist[len(dist)-1][len(dist[0]-1)]
+}
+
+func Matrix(len1, len2 int) [][]int {
+	arr := make([][]int, len1)
+
+	for i := 0; i < len1; i++ {
+		arr[i] = make([]int, len2)
+	}
+	return arr
+}
+
+func min(ur ...int) int {
+	if len(ur) < 1 {
+		panic("Input slice length is too little")
+	}
+	var least int
+	for i, val := range ur {
+		if i == 0 || val < least {
+			least = val
+		}
+	}
+	return least
 }
--- src/Alignment/editDistanceMatrix.go
+++ src/Alignment/editDistanceMatrix.go
@@ -3,6 +3,48 @@ package main
 //EditDistanceMatrix takes as input a slice of strings patterns.
 //It returns a matrix whose (i,j)th entry is the edit distance between
 //the i-th and j-th strings in patterns.
+/*Code Challenge: Implement a function EditDistanceMatrix(patterns).
+
+Input: an array of strings, patterns.
+Return: a matrix E such that E[i][j] is the edit distance between the i-th string and the j-th strings in patterns.
+Functions that are implemented behind the scenes for you:
+
+EditDistance(): takes as input two strings, and returns the edit distance between the two strings.
+
+Sample Output 4:
+
+0 2 2 2
+2 0 2 2
+2 2 0 2
+2 2 2 0
+Sample Input 5:
+
+AC CT TG GC AG GA GAT CAT ACG GAC
+Sample Output 5:
+
+0 2 2 1 1 2 2 2 1 1
+2 0 2 2 2 2 2 1 2 3
+2 2 0 2 1 2 3 3 2 3
+1 2 2 0 2 1 2 3 2 1
+1 2 1 2 0 2 2 2 1 2
+2 2 2 1 2 0 1 2 3 1
+2 2 3 2 2 1 0 1 3 1
+2 1 3 3 2 2 1 0 3 2
+1 2 2 2 1 3 3 3 0 2
+1 3 3 1 2 1 1 2 2 0 */
+
 func EditDistanceMatrix(patterns []string) [][]int {
-	return [][]int{}
+
+	//make a matrix storing the values of editDistances
+	stor := Matrix(len(array), len(array))
+	//nested for loops calculating distance between different array[] values
+	for i := 0; i < len(array); i++ {
+
+		for j := 0; j < len(array); j++ {
+			stor[i][j] = EditDistance(array[i], array[j])
+
+		}
+
+	}
+	return stor
 }
