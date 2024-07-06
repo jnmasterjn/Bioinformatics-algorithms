54163c1dc5b98d546d965b85a283e3929765243d
Janice <janice0975751227@gmail.com>
2024-07-05 21:08:22 -0400

    string shuffle

--- src/Assembly/string_shuffle.go
+++ src/Assembly/string_shuffle.go
@@ -1,7 +1,24 @@
 package main
 
+import (
+	"math/rand"
+)
+
 //ShuffleStrings takes a collection of strings patterns as input.
 //It returns a random shuffle of the strings.
+
 func ShuffleStrings(patterns []string) []string {
-	return []string{}
+
+	random_generator := rand.Perm(len(patterns)) //store index
+	shuffle := make([]string, len(patterns))     //store value
+
+	for i := 0; i < len(random_generator); i++ {
+		for u := 0; u < len(patterns); u++ { //nested loop to find all posible positions
+			if random_generator[i] == u {
+				shuffle[i] = patterns[u]
+			}
+		}
+	}
+
+	return shuffle
 }
