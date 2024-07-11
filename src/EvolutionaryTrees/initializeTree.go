12a31c8d7be2927dc2e4ba5929415792a787ea63
Janice <janice0975751227@gmail.com>
2024-07-11 10:45:25 -0400

    adding sequencing files 16s genes

--- src/EvolutionaryTrees/initializeTree.go
+++ src/EvolutionaryTrees/initializeTree.go
@@ -1,14 +1,25 @@
 package main
 
-import (
-	"strconv"
-)
-
-//InitializeTree takes a slice of n species names as input.
-//It returns a rooted binary tree with with 2n-1 total nodes, where
-//the leaves are the first n and have the associated species names.
+// InitializeTree takes a slice of n species names as input.
+// It returns a rooted binary tree with with 2n-1 total nodes, where
+// the leaves are the first n and have the associated species names.
 func InitializeTree(speciesNames []string) Tree {
 	var t Tree
+
+	numLeaves := len(speciesNames)
+
+	if numLeaves == 0 {
+		panic("Error: no speices given")
+	}
+
+	t = make([]Node, 2*numLeaves-1)
+
+	for i := range t {
+		//decleare a node
+		var vx Node
+
+		t[i] = &vx
+	}
 
 	return t
 }
